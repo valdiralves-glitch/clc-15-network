2fbe0fe372e195cf26ac9d7fe8cdc8e3715df911
Valdir Silva <tux.valdir.alves@aluno.impacta.edu.br>
2026-06-10 20:00:29 +0000

    Correção das Vulnerabilidades

--- main.tf
+++ main.tf
@@ -8,6 +8,23 @@ resource "aws_vpc" "terraform_vpc" {
   }
 }
 
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::clc-15-valdir-terraform"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.terraform_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
+
 ## Cria as 4 subnets em suas respectivas AZs ##
 ## Cria subnets na AZ 1A
 resource "aws_subnet" "subnet_public_1a" {
